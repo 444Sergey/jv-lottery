f45bbe033f603813360db47ce2e387e6e9c018fc
Sergey <shybko2018@gmail.com>
2026-01-18 16:52:04 +0100

    Implementation-of-comments-from-the-Elena-Bruyako

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,14 +3,12 @@ package core.basesyntax;
 import java.util.Arrays;
 
 public class Application {
-    private static final Lottery lottery = new Lottery();
-    private static final Ball [] balls = new Ball[3];
+    private static final int NUMBER_OF_BALLS = 3;
 
     public static void main(String[] args) {
-        for (int i = 0; i < balls.length; i++) {
-            balls[i] = lottery.getRandomBall();
+        Lottery lottery = new Lottery();
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
+            System.out.println(lottery.getRandomBall());
         }
-        System.out.println(Arrays.toString(balls));
-
     }
 }
